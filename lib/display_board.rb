05f0c59c818b81448131d3d2bc9cdf6c023300a1
valeria campilongo <cvaleriac@hotmail.it>
2018-02-23 23:54:14 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,7 @@
 def display_board
-  board = [
   puts "  "   "  | "   "  | "   " "
   puts " ----------- "
   puts "  "   "  | "   "  | "   " "
   puts " ----------- "
-  puts "  "   "  | "   "  | "   " "]
+  puts "  "   "  | "   "  | "   " "
 end

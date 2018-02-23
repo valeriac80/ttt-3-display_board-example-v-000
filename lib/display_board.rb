6ec25a09ed86d1a4b445441974e159352683f693
valeria campilongo <cvaleriac@hotmail.it>
2018-02-23 23:22:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 def display_board
-  phrase = "A tic tac toe board"
   puts "  "   "  | "   "  | "   " "
   puts " ----------- "
   puts "  "   "  | "   "  | "   " "
   puts " ----------- "
   puts "  "   "  | "   "  | "   " "
+  print "A tic tac toe board"
 end
